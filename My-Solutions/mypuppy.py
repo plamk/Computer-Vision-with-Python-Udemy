2ff94afb496d68c25d3c2da5a40f7a7f99a4eaea
plamk <plamenakoleva52@gmail.com>
2023-03-23 12:21:23 +0100

    script to see a image on new window

---
+++ My-Solutions/mypuppy.py
@@ -0,0 +1,14 @@
+#Before install opencv on Terminal: $ pip install opencv-python
+
+import cv2
+
+img= cv2.imread('../DATA/00-puppy.jpg')
+
+while True:
+    cv2.imshow('Puppy',img)
+
+    # IF we've waited at least 1 ms AND we've pressed rhe Esc key by default
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
