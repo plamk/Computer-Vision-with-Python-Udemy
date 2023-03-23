5d2ff82a777152e63816cb953e255464ef3b21be
plamk <plamenakoleva52@gmail.com>
2023-03-23 17:24:18 +0100

    fix: winname of script

--- My-Solutions/S01-Image-Basics-with-OpenCV/puppyCircle.py
+++ My-Solutions/S01-Image-Basics-with-OpenCV/puppyCircle.py
@@ -8,15 +8,15 @@ def draw_circle(event,x,y,flags,parm):
     if event == cv2.EVENT_RBUTTONDOWN:
         cv2.circle(img, (x,y), 100, (0,0,255),thickness=8)
 
-cv2.namedWindow(winname='my_drawing')
-cv2.setMouseCallback('my_drawing', draw_circle)
+cv2.namedWindow(winname='puppy_dog')
+cv2.setMouseCallback('puppy_dog', draw_circle)
 
 ##### SHOWING IMAGE WITH OPENCV #####
 img = cv2.imread('../../DATA/dog_backpack.jpg')
 fix_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 while True:
-    cv2.imshow('my_drawing',img)
+    cv2.imshow('puppy_dog',img)
 
     # IF we've waited at least 1 ms AND we've pressed rhe Esc key by default
     if cv2.waitKey(1) & 0xFF == 27:
